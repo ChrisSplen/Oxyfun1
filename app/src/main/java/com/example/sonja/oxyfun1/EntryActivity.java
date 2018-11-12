ef4889735ef72f612754592927d58f0945f263c9
ChrisSplen <44968388+ChrisSplen@users.noreply.github.com>
2018-11-12 17:14:33 +0100

    Update EntryActivity.java

--- app/src/main/java/com/example/sonja/oxyfun1/EntryActivity.java
+++ app/src/main/java/com/example/sonja/oxyfun1/EntryActivity.java
@@ -13,7 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class EntryActivity extends AppCompatActivity {
-//just a comment
+//just a comment or is it?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

a3567277fe13e3c7c9c78ccb58670dbc675a4b52
preiju98 <45006407+preiju98@users.noreply.github.com>
2019-01-14 23:38:57 +0100

    Konflikte in layout activity_main behoben. Button der Einstellungen dient jetzt zum starten der Datei laden Aktivität.

--- app/src/main/java/com/example/sonja/oxyfun1/MainActivity.java
+++ app/src/main/java/com/example/sonja/oxyfun1/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.support.v7.widget.Toolbar;
 
+
 import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
